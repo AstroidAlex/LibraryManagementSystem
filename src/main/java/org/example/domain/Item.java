cfad0956af042fccb9febb3affbef95329e7892a
Alex <maxelantra@gmail.com>
2026-05-12 14:14:06 -0400

    Items class has basics, lacks exception handling. Added Lombok, as I forgot it.

--- src/main/java/org/example/domain/Item.java
+++ src/main/java/org/example/domain/Item.java
@@ -1,4 +1,38 @@
 package org.example.domain;
 
-public class Item {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Item {
+    private final String id;
+    private String title;
+    private ItemStatus status;
+
+    private static int nextId = 1;
+
+    public Item(String title, ItemStatus status) {
+        this.id = String.format("%04d", nextId++); //TODO: adapt for different types
+        this.title = title;
+        this.status = status;
+    }
+
+    public void returnItem() {
+        status = ItemStatus.AVAILABLE;
+    }
+    public void markLost() {
+        status = ItemStatus.LOST;
+    }
+
+    /**
+     * can be used when adding new items
+     */
+    public boolean isAvailable() {
+        return status == ItemStatus.AVAILABLE;
+    }
+
+    public enum ItemStatus {
+        AVAILABLE, BORROWED, LOST
+    }
 }
