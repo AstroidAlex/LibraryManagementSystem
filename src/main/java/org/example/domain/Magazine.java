c0cb76ef76858eecfbcf710f871ade1d04d8042a
Alex <maxelantra@gmail.com>
2026-05-12 14:21:38 -0400

    Forgot to add Magaizine. Warnings temporarly ignored.

--- src/main/java/org/example/domain/Magazine.java
+++ src/main/java/org/example/domain/Magazine.java
@@ -1,4 +1,17 @@
 package org.example.domain;
 
-public class Magazine {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@Getter
+public class Magazine extends Item{
+    private final String issueNumber;
+    private final String publisher;
+
+    public Magazine(String title, ItemStatus status, String issueNumber, String publisher) {
+        super(title, status);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
